0976ec57c23f8ac0e7adae3f4413abc39b1f3d33
Sergei Shmanko <admin@phoenix.ee>
2016-05-22 20:49:27 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='WebApp',
+      version='0.1',
+      description='Simple Login App',
+      author='Sergei Shmanko',
+      author_email='a@a.com',
+      url='https://github.com/sshmanko/webapp',
+      install_requires=['Flask-User'],
+      )

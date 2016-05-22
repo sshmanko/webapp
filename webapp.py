29d85a04c54f496342f3bfcb92996fad10a5b05e
Sergei Shmanko <admin@phoenix.ee>
2016-05-22 23:11:09 +0300

    Modify page template, move config to separate file

--- webapp.py
+++ webapp.py
@@ -4,34 +4,12 @@ from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter
 
-
-# Use a Class-based config to avoid needing a 2nd file
-# os.getenv() enables configuration through OS environment variables
-class ConfigClass(object):
-    # Flask settings
-    SECRET_KEY =              os.getenv('SECRET_KEY',       'THIS IS AN INSECURE SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     'sqlite:///basic_app.sqlite')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CSRF_ENABLED = True
-
-    # Flask-Mail settings
-    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        'email@example.com')
-    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        'password')
-    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"MyApp" <noreply@example.com>')
-    MAIL_SERVER =             os.getenv('MAIL_SERVER',          'smtp.gmail.com')
-    MAIL_PORT =           int(os.getenv('MAIL_PORT',            '465'))
-    MAIL_USE_SSL =        int(os.getenv('MAIL_USE_SSL',         True))
-
-    # Flask-User settings
-    USER_APP_NAME        = "AppName"                # Used by email templates
-
-
 def create_app():
     """ Flask application factory """
     
     # Setup Flask app and app.config
     app = Flask(__name__)
-    app.config.from_object(__name__+'.ConfigClass')
+    app.config.from_pyfile('webapp.cfg')
 
     # Initialize Flask extensions
     db = SQLAlchemy(app)                            # Initialize Flask-SQLAlchemy
